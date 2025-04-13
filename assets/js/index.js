875a92d88c440821561bf2b273a84c817d7b0874
Luna <ludmilamodna3@gmail.com>
2025-04-13 12:23:27 +0300

    Added counter

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,55 +1,24 @@
-"use strict";
-const slides = [
-  {
-    src: "https://images.pexels.com/photos/624015/pexels-photo-624015.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    alt: "landscape1",
-  },
-  {
-    src: "https://images.pexels.com/photos/1287145/pexels-photo-1287145.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    alt: "landscape2",
-  },
-  {
-    src: "https://images.pexels.com/photos/371589/pexels-photo-371589.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    alt: "landscape3",
-  },
-  {
-    src: "https://images.pexels.com/photos/417074/pexels-photo-417074.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    alt: "landscape4",
-  },
-];
+//отримуємо необхідні посилання
+const [decBtn, incBtn] = document.querySelectorAll(".counterBtn");
+const counterEl = document.querySelector("#counter");
 
-const slideImg = document.querySelector("img");
+//задаємо стан
+let counter = 0;
 
-const [prevBtn, nextBtn] = document.querySelectorAll(".navBtn");
+//задаємо значення елемента на сторінці
+counterEl.textContent = counter;
 
-try {
-  // Виносимо відповідальність за обчислення для слайдера на клас
-  const slider = new Slider(slides);
-
-  updateSlideImage(slider.currentSlide);
-
-  function nextSlideHandler() {
-    slider.incSlideIndex();
-    updateSlideImage(slider.currentSlide);
-  }
-  nextBtn.addEventListener("click", nextSlideHandler);
-
-  function prevSlideHandler() {
-    slider.decSlideIndex();
-    updateSlideImage(slider.currentSlide);
-  }
-  prevBtn.addEventListener("click", prevSlideHandler);
-  
-  function updateSlideImage(currentSlide) {
-    // currentSlide - об'єкт {src, alt}
-    slideImg.src = currentSlide.src;
-    slideImg.alt = currentSlide.alt;
-    slideImg.onerror = () => {
-      slideImg.src =
-        "https://media.istockphoto.com/id/1403500817/photo/the-craggies-in-the-blue-ridge-mountains.jpg?s=612x612&w=0&k=20&c=N-pGA8OClRVDzRfj_9AqANnOaDS3devZWwrQNwZuDSk=";
-    };
-  }
-} catch (e) {
-  slideImg.src =
-    "https://media.istockphoto.com/id/1403500817/photo/the-craggies-in-the-blue-ridge-mountains.jpg?s=612x612&w=0&k=20&c=N-pGA8OClRVDzRfj_9AqANnOaDS3devZWwrQNwZuDSk=";
+//додаємо 2 обробника події на - і +
+function decCount(e) {
+  counter--;
+  counterEl.textContent = counter;
 }
+
+decBtn.addEventListener("click", decCount);
+
+function incCount(e) {
+  counter++;
+  counterEl.textContent = counter;
+}
+
+incBtn.addEventListener("click", incCount);
